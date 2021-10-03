3efc29eefc567be5aa109f5bcc5b6bf8acfff9a0
anCRONIK <ancronik@gmail.com>
2021-10-03 16:17:23 +0200

    upgraded security

--- src/main/java/site/ancronik/movie/trailer/searcher/security/config/CustomUserDetailsService.java
+++ src/main/java/site/ancronik/movie/trailer/searcher/security/config/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package site.ancronik.movie.trailer.searcher.security.config;
 
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,10 +12,16 @@ import java.util.List;
 
 public class CustomUserDetailsService implements UserDetailsService { //TODO change to JDBC or some other implementation
 
-    private final PasswordEncoder passwordEncoder;
+    private static final String KEY_USER_PASSWORD = "user.password";
+    private static final String KEY_ADMIN_PASSWORD = "admin.password";
+    private static final String DEFAULT_PASSWORD = "password";
 
-    public CustomUserDetailsService(PasswordEncoder passwordEncoder) {
+    private final PasswordEncoder passwordEncoder;
+    private final Environment environment;
+
+    public CustomUserDetailsService(PasswordEncoder passwordEncoder, Environment environment) {
         this.passwordEncoder = passwordEncoder;
+        this.environment = environment;
     }
 
     @Override
@@ -32,9 +39,9 @@ public class CustomUserDetailsService implements UserDetailsService { //TODO cha
     public List<UserDetails> populateUserDetails() {
         List<UserDetails> userDetailsList = new ArrayList<>();
         userDetailsList
-            .add(User.withUsername("user").password(passwordEncoder.encode("password")).roles("USER").build());
+            .add(User.withUsername("user").password(passwordEncoder.encode(environment.getProperty(KEY_USER_PASSWORD, DEFAULT_PASSWORD))).roles("USER").build());
         userDetailsList
-            .add(User.withUsername("admin").password(passwordEncoder.encode("password")).roles("USER", "ADMIN").build());
+            .add(User.withUsername("admin").password(passwordEncoder.encode(environment.getProperty(KEY_ADMIN_PASSWORD, DEFAULT_PASSWORD))).roles("USER", "ADMIN").build());
 
         return userDetailsList;
     }
--- src/main/java/site/ancronik/movie/trailer/searcher/security/config/SecurityConfig.java
+++ src/main/java/site/ancronik/movie/trailer/searcher/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package site.ancronik.movie.trailer.searcher.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,8 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public CustomUserDetailsService customUserDetailsService(PasswordEncoder passwordEncoder) {
-        return new CustomUserDetailsService(passwordEncoder);
+    public CustomUserDetailsService customUserDetailsService(PasswordEncoder passwordEncoder, Environment environment) {
+        return new CustomUserDetailsService(passwordEncoder, environment);
     }
 
     @Override
