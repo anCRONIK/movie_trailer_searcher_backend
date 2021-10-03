8eb107849237e3228e6cfcbd05faa7b2f8169f8d
anCRONIK <ancronik@gmail.com>
2021-10-03 15:23:16 +0200

    fix test

--- src/main/java/site/ancronik/movie/trailer/searcher/security/config/SecurityConfig.java
+++ src/main/java/site/ancronik/movie/trailer/searcher/security/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/").permitAll()
+            .antMatchers("/", "/error").permitAll()
             .anyRequest().authenticated()
             .and().httpBasic().and().csrf().disable();
     }
